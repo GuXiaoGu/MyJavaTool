7097d3d3b0db4615e8c2ab2bb1db608ee84f21f7
黄顿迪 <1540848915@qq.com>
2023-07-12 13:05:59 +0800

    002：测试

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package main.java.org.example;
+
+/**
+ * @Author: hdd
+ * @Date: ${DATE} - ${TIME}
+ * @Description: org.example
+ * @version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
